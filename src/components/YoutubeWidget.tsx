44df4dd8a49cc235d4eadfb3c6b9983a7f2951f3
Broque Thomas <batmanesignal@gmail.com>
2025-05-27 19:52:42 -0700

    remove exposed api key

--- src/components/YoutubeWidget.tsx
+++ src/components/YoutubeWidget.tsx
@@ -5,7 +5,7 @@ import React, { useState, FormEvent, useEffect, useCallback} from 'react'; // En
 import Image from 'next/image';
 
 // --- Constants ---
-const YOUTUBE_API_KEY: string = 'AIzaSyAIsr27eVlsTIgd0kLM9Lq_WDr-vGbJjtI'; // User provided key. WARNING: Exposing API keys on the client side is insecure.
+const YOUTUBE_API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY // User provided key. WARNING: Exposing API keys on the client side is insecure.
 
 // --- Icons ---
 const SearchIcon = () => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z" clipRule="evenodd" /></svg>;
